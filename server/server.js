58df44520f4529874bbe4f083db3f86e7a11dd32
nvalle0624 <nvalle0624@gmail.com>
2021-09-11 21:54:39 -0400

    testing new port

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen(9000, () => console.log("Server Running"));
+app.listen(8080, () => console.log("Server Running"));
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
