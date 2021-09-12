acf94327bf7aabf73bbb2755ab5e670c6b71694d
nvalle0624 <nvalle0624@gmail.com>
2021-09-11 22:06:32 -0400

    testing new port

--- src/components/contact_page/Contact.js
+++ src/components/contact_page/Contact.js
@@ -14,7 +14,7 @@ const ContactForm = () => {
       email: email.value,
       message: message.value,
     };
-    let response = await fetch("https://localhost:8080/contact", {
+    let response = await fetch("http://localhost:8080/contact", {
       method: "POST",
       headers: {
         "Content-Type": "application/json;charset=utf-8",
