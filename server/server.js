30d716dc380ba871a5212ee26e73d481f01b16a7
nvalle0624 <nvalle0624@gmail.com>
2021-09-12 09:26:49 -0400

    testing cors

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use("/", router);
 app.listen(8080, () => console.log("Server Running"));
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://nicholasvallefsse.com");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
