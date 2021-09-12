ec4e0057944b300ed17edd112eb7afd3ec934a32
nvalle0624 <nvalle0624@gmail.com>
2021-09-12 09:42:15 -0400

    testing cors

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const nodemailer = require("nodemailer");
 const app = express();
 app.use(cors());
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://nicholasvallefsse.com");
+  res.header("Access-Control-Allow-Origin", "https://nicholasvallefsse.com/contact");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, POST");
   res.header("Access-Control-Allow-Credentials", true);
